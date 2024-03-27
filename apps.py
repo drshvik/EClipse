b18290cfb7cbb3ee41da7d8f5fcfdf0d1c9e7401
drshvik <142541640+drshvik@users.noreply.github.com>
2024-03-27 19:04:59 +0530

    Delete apps.py

--- apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-import users
-
-class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
-
-    def ready(self):
-        from users import signals
